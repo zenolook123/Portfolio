29cb18b9e13815ecba3da0bdf57755e0e40aa610
Zach Garrison <zgedina@yahoo.com>
2024-01-17 22:37:00 -0600

    gif guy

--- src/Components/WelcomeScreen/WelcomeScreen.jsx
+++ src/Components/WelcomeScreen/WelcomeScreen.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, Typography, IconButton } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { motion } from 'framer-motion';
+import './WelcomeScreen.css';
 
 const WelcomeScreen = () => {
   const handleScroll = () => {
@@ -12,21 +13,21 @@ const WelcomeScreen = () => {
   };
 
   return (
-    <Box sx={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: 'darkgray' }}>
+    <Box className="welcome-screen" sx={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
       <motion.div
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 5 }}
+        transition={{ duration: 2 }}
       >
-        <Typography variant="h2" component="h1" color="text.primary">
+        <Typography variant="h2" component="h1" className="welcome-text">
           Zach Garrison
         </Typography>
-        <Typography variant="h4" component="h2" color="text.primary">
+        <Typography variant="h4" component="h2" className="welcome-text">
           Software Developer
         </Typography>
       </motion.div>
-      <IconButton onClick={handleScroll} sx={{ position: 'absolute', bottom: 20, color: 'white' }}>
-        <KeyboardArrowDownIcon style={{ fontSize: 60 }} />
+      <IconButton className="IconButton" onClick={handleScroll}>
+        <KeyboardArrowDownIcon style={{ fontSize: 80, color:'white' }} />
       </IconButton>
     </Box>
   );
